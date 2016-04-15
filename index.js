14638b24e259b3f80fdb46b09da5f34d16424507
Daniel <danieldelacruz01@gmail.com>
2016-04-15 17:48:27 +1200

    add checkpoint component

--- index.js
+++ index.js
@@ -3,22 +3,15 @@ import { render } from 'react-dom'
 import App from './modules/App'
 import { Router, Route, browserHistory, IndexRoute } from 'react-router'
 import About from './modules/About'
-import Trails from './modules/Trails'
 import Trail from './modules/Trail'
 import Home from './modules/Home'
-
-
 
 render((
   <Router history={browserHistory}>
     <Route path="/" component={App}>
-    <IndexRoute component={Home}/>
-      <Route path="/trails" component={Trails}>
-        <Route path="/trails/:userName/:trailName" component={Trail}/>
-      </Route>
-      <Route path="/about" component={About}>
-        <Route path="/trail" component={Trail}/>
-      </Route>
+      <indexRoute component={Home}/>
+      <Route path="/about" component={About}/>
+      <Route path="/trail" component={Trail}/>
     </Route>
   </Router>
 ), document.getElementById('app'))
