2bab5b505573377cafeba059e9b6a2fa2bd28cbd
Dominic Taylor <email": null>
2016-04-16 20:58:12 +1200

    Add doms latest work

--- models/location.js
+++ models/location.js
@@ -23,17 +23,21 @@ function getUserLocation() {
 
 function verifyUserPosition(checkpointCoords){
   var promise = new Promise(function(resolve,reject){
+    console.log('hoiuhiojoi')
     var range = 0.00005
     getUserLocation()
       .then(function(userCoords){
         console.log('VUP', userCoords)
-      })
-      .catch(function(error){})
-
+        console.log((userCoords.latitude <= checkpointCoords.latitude+range && userCoords.latitude >= checkpointCoords.latitude-range) &&
+      (userCoords.longitude <= checkpointCoords.longitude+range && userCoords.longitude >= checkpointCoords.longitude-range))
     resolve(
       (userCoords.latitude <= checkpointCoords.latitude+range && userCoords.latitude >= checkpointCoords.latitude-range) &&
       (userCoords.longitude <= checkpointCoords.longitude+range && userCoords.longitude >= checkpointCoords.longitude-range)
     )
+
+      })
+      .catch(function(error){})
+
   })
 }
 
