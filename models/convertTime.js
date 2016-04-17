0462bd319fea8eae7bd1aa832c49d67b351460b1
lou <email": null>
2016-04-18 09:52:58 +1200

    add get hours and get seconds to convertToTime fn

--- models/convertTime.js
+++ models/convertTime.js
@@ -1,15 +1,25 @@
+//time is either start or end timestamp passsed in
 function convertToMinutes(time){
   var date = new Date();
+  var hour = date.getHours();
   var minutes = date.getMinutes();
-  if (minutes<10){
-  console.log("Your time is 0", + minutes + " minutes")
+  var seconds = date.getSeconds();
+  if (hour<10 ) {
+    hour = "0" + hour;
+    if(minutes<10){
+      minutes = "0" + minutes;
+      if(seconds<10){
+        seconds = "0" + seconds;
+      }
+    }
+    console.log("Your time is " + hour + ":" + minutes + ":" + seconds)
   }
   else {
-  console.log("Your time is", + minutes + " minutes")
+  console.log("Your time is " + hour + ":" + minutes + ":" + seconds)
   }
 }
-//uncomment to test
-//convertToMinutes(1460880744012)
+
+convertToMinutes(1460880744012)
 
 
 
