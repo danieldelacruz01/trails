fdd7e0e93d0613a7fc39966a8ae22fdf2c24ee29
lou <email": null>
2016-04-18 18:41:31 +1200

    Add leaderboard endpoint, new migrations and seed

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -4,10 +4,12 @@ exports.seed = function(knex, Promise) {
     knex('runs').del(),
 
     // Inserts seed entries
-    knex('runs').insert({runId: 1, startTime: 1460857277992, endTime:1460857524363, name:'Daniel'}),
-    knex('runs').insert({runId: 2, startTime: 1460857324960, endTime:1460857547881, name: 'Dominic'}),
-    knex('runs').insert({runId: 3, startTime: 1460857343284, endTime:1460857506429, name: 'Louise'})
+    knex('runs').insert({runId: 1, startTime: 1460946600000, endTime:1460953800000, name:'Daniel',trailTime:'2:00:00'}),
+    knex('runs').insert({runId: 2, startTime: 1460946600000, endTime:1460954100000, name: 'Dominic', trailTime:'2:05:00'}),
+    knex('runs').insert({runId: 3, startTime: 1460946600000, endTime: 1460952300000, name: 'Louise', trailTime:'1:55:00'}),
+    knex('runs').insert({runId: 4, startTime: 1460946600000, endTime:1460954160000, name:'Piet',trailTime:'2:06:00'}),
+    knex('runs').insert({runId: 5, startTime: 1460946600000, endTime:1460953860000, name: 'Simon', trailTime:'2:01:00'}),
+    knex('runs').insert({runId: 6, startTime: 1460946600000, endTime:1460954640000, name: 'Mix', trailTime:'2:11:00'})
   );
-  return console.log("hi, seeds are planted")
 };
-console.log("seeded")
+
