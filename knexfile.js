1fed3a500c18b7d76966c7602fda54b28b26cebb
Daniel <danieldelacruz01@gmail.com>
2016-05-27 14:41:18 +1200

    add database url to knexfile

--- knexfile.js
+++ knexfile.js
@@ -35,19 +35,6 @@ module.exports = {
     }
   },
 
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user: 'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
+  production: process.env.DATABASE_URL 
+  
 }
